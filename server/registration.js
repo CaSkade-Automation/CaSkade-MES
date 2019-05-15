9190287eade3c6bc89a4ed86c359dddca1e25887
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2019-05-15 15:02:33 +0200

    Simplified registration. Sending broadcast response instead of sending new POST

--- server/registration.js
+++ server/registration.js
@@ -18,33 +18,41 @@ udpClient.bind(PORT, HOST, function () {
 udpClient.on('message', function (message, remote) {
   console.log('Broadcast Msg: From: ' + remote.address + ':' + remote.port + '\nMessage: ' + message);
 
-  // parse message to get module self description and address
+  // parse message to get the infos that are necessary for registration
   let moduleBroadCast = JSON.parse(message)
   let modulePort = moduleBroadCast['port'];
-  moduleAddress = `http://${remote.address}:${modulePort}`;
-  //let opsRegistrationLocation = moduleBroadCast.getFunctionByName('ops-registration').location; 
-  let opsRegistrationLocation = moduleBroadCast['location']; 
-  
+  moduleAddress = `http://${remote.address}`;
+  let selfDescriptionEndpoint = moduleBroadCast['self-description-endpoint']; 
 
-  // create OPS AAS
-  opsSelfDescription = new SelfDescription(123, 'Order Processing System', 9090, [
-    new ModuleFunction('Order-Management', 'Create new Orders', '/api/order-management', 'POST', [{'name':'customerName', 'dataType': 'string'}, {'name': 'productModel', 'dataType' : 'file'}]),
-    new ModuleFunction('KPI-Dashboard', 'Allows for calculation and display of KPIs', '/api/kpi-calculation', 'POST', [{'name':'nameOfKpi', 'dataType' : 'string'}]),
-    new ModuleFunction('Module-Registration', 'Register new modules at this endpoint', '/api/modules', 'POST', [{'name':'moduleSelfDescription', 'dataType' : 'SelfDescription'}])
-  ]);
-  
-  // 
 
-  // send the request
-  request.post({
-      url: encodeURI(moduleAddress + opsRegistrationLocation),
-      json: opsSelfDescription
-    },
-    function (error, response, body) {
-      console.log('error: ' + error);
-      console.log('body' + body);
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    });
+  // Create the answer (specifying registration endpoint)
+  let broadcastAnswer = {
+    "id": 1,
+    "port": 9090,
+    "self-description-endpoint": "/self-descriptions"
+  };
+
+
+  udpClient.send(JSON.stringify(broadcastAnswer), remote.port, remote.address, function(err) {
+    console.log(`Sending to address: ${remote.address}`);
+    console.log(`Sending to port: ${remote.port}`);
+    if (err) {
+      console.log(`Error while answering broadcast, ${err}`);
+    }
+    else console.log(`Sent broadcast answer to ${moduleAddress}`);
+  });
+  
+  
+  // // send the request to the module
+  // request.post({
+  //   url: encodeURI(moduleAddress + opsRegistrationLocation),
+  //   json: opsSelfDescription
+  // },
+  // function (error, response, body) {
+  //   console.log('error: ' + error);
+  //   console.log('body' + body);
+  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // });
 
 
 });
