06a08f7e8b3ff826e03579cf362559f1848f1d31
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2019-06-04 09:26:28 +0200

    Deleted some old elements of the template that were no longer needed

--- frontend/app/layout/layout-routing.module.ts
+++ frontend/app/layout/layout-routing.module.ts
@@ -14,11 +14,6 @@ const routes: Routes = [
             { path: 'reconfiguration-overview', loadChildren: '../own-modules/reconfiguration-overview/reconfiguration-overview.module#ReconfigurationOverviewModule'},
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
     }
