a7181c1b6d47df5822ecbf0fe565b72b74acbdd9
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2019-12-12 10:01:11 +0100

    Just a little cleanup.

--- server/socket-server.js
+++ server/socket-server.js
@@ -15,7 +15,7 @@ class SocketServer {
       });
     });
   }
-  
+
 
   emitNewSelfDescription (selfDescription) {
     this.io.emit('moduleregistration', selfDescription);
@@ -33,37 +33,3 @@ class SocketServer {
 }
 
 module.exports = SocketServer;
-
-
-// module.exports = function (server) {
-
-//   var io = require('socket.io').listen(server);
-
-//   io.on('connection', (socket) => {
-//     socket.on('message', (msg) => {
-//       console.log('message: %s', JSON.stringify(msg));
-
-//         // create new example aas
-//         myAas = {
-//             "id": 123,
-//             "name": "myModule", 
-//             "submodels":[
-//                 {
-//                     "type": "ontology",
-//                     "name": "Service-Ontology"
-//                 },
-//                 {
-//                     "type": "ontology",
-//                     "name": "KPI-Ontology"
-//                 }
-//             ]};
-
-//       io.emit('module-registration', msg = myAas);
-//     });
-//   });
-
-//   emitNewSelfDescription (selfDescription) {
-//     io.emit('module-registration', msg = selfDescription);
-//   };
-
-// }
