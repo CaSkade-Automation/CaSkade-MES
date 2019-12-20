ded408c37317db6e431fcc21cfab32daa16b71ea
Tom Jeleniewski <jeleniewskitom@gmail.com>
2019-12-20 10:49:21 +0100

    Added delete function for capabilities
    Service-management renamed in capability-management. Added function for deleting a capability by its IRI

---
+++ server/routes/capability-management.route.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var QueryMapper = require('../models/selfdescription/GraphQueryMapper')
+var queryMapper = new QueryMapper();
+const uuidv4 = require('uuid/v4');
+
+module.exports = function (socketServer, graphDbConnection) {
+
+
+
+  
+// **DELETE A CAPABILITY**
+
+router.delete('/:capabilityIri', function(req, res){
+  const capabilityIri = decodeURIComponent(req.params['capabilityIri']);
+  
+// Query to get capability by IRI
+  const query = `PREFIX VDI3682: <http://www.hsu-ifa.de/ontologies/VDI3682#>
+    SELECT ?process ?graph WHERE {
+        GRAPH ?graph {
+
+            BIND(IRI("${capabilityIri}") AS ?process).
+            
+            {
+            ?process a VDI3682:Process.
+            } 
+        }
+    }`;
+    // execute query
+    graphDbConnection.executeQuery(query)
+    .then((dbResult) => {
+      const dbResultBindings = dbResult.results.bindings;
+      dbResultBindings.forEach(bindings => {
+        const graphName = bindings.graph.value;
+
+        // clear every graph
+        graphDbConnection.clearGraph(graphName);
+      });
+      res.status(204).end();
+    })
+    .catch((err) => {
+      res.status(400).json(
+        {"msg": "Error while trying to delete a capability by its IRI",
+        "err": err,
+        }
+      );
+    })
+});
+ return router;
+};
+
+
+
+
+
+
+
