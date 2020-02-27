c79abebb859f668910199c8b598bc643eb09328b
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-02-27 18:49:05 +0100

    First draft of interfaces and classes for relevant elements

---
+++ frontend/app/shared/models/module.ts
@@ -0,0 +1,31 @@
+export interface Module {
+  name: string;
+  capabilities: Capability[];
+}
+
+export interface Capability {
+  name: string;
+  hasInput: FpbState[];
+  hasOutput: FpbState[];
+  executableViaSkill: Skill[];
+}
+
+
+// Interface for Energy, Information and Product
+// VDI3682 calls them "State"
+export interface FpbState {
+  name: string;
+  stateType: string;    // Defines whether it's a product, energy, resource
+}
+
+export interface Skill {
+  name: string;
+}
+
+export interface RestSkill {
+
+}
+
+export interface OpcUaSkill {
+
+}
