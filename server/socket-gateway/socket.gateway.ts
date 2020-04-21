4a1e754b6e11c8447ddde3d9979e2c621193977b
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-04-21 11:57:51 +0200

    Added global modules for graphDbConnection and socket gateway

---
+++ server/socket-gateway/socket.gateway.ts
@@ -0,0 +1,32 @@
+import {OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse} from '@nestjs/websockets';
+import {Client, Server} from 'socket.io';
+
+@WebSocketGateway()
+export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() server: Server;
+  connectedClients: number = 0;
+
+  handleConnection(client: any, ...args: any[]) {
+    this.connectedClients++;
+  }
+
+  handleDisconnect(client: any) {
+    this.connectedClients--;
+  }
+
+
+  // emitNewSelfDescription (selfDescription) {
+  //   this.io.emit('moduleregistration', selfDescription);
+  // };
+
+
+  /**
+   * Emits a mesage with the 'moduleregistration'-event
+   * @param {string} message The message to be emitted
+   */
+  emitModuleRegistrationEvent(message) {
+    this.server.emit('moduleregistration', message);
+  }
+
+}
