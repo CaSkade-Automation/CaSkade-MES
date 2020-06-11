6b0cf74eb41dbcd41ae84817d66b44a99a03e3c2
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-06-11 14:26:56 +0200

    Angular update and related settings

--- frontend/src/app-routing.module.ts
+++ frontend/src/app-routing.module.ts
@@ -4,14 +4,15 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 
 const routes: Routes = [
-    { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
-    { path: 'login', loadChildren: './modules/login/login.module#LoginModule' },
-    { path: 'signup', loadChildren: './modules/signup/signup.module#SignupModule' },
-    { path: 'error', loadChildren: './modules/server-error/server-error.module#ServerErrorModule' },
-    { path: 'access-denied', loadChildren: './modules/access-denied/access-denied.module#AccessDeniedModule' },
-    { path: 'not-found', loadChildren: './modules/not-found/not-found.module#NotFoundModule' },
+    { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule), canActivate: [AuthGuard]},
+    { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
+    { path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule)},
+    { path: 'error', loadChildren: () => import('./modules/server-error/server-error.module').then(m => m.ServerErrorModule)},
+    { path: 'access-denied', loadChildren: () => import('./modules/access-denied/access-denied.module').then(m => m.AccessDeniedModule)},
+    { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule)},
     { path: '**', redirectTo: 'not-found' }
 ];
+
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- frontend/src/layout/layout-routing.module.ts
+++ frontend/src/layout/layout-routing.module.ts
@@ -8,17 +8,18 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
-            { path: 'module-management', loadChildren: '../modules/module-management/module-management.module#ModuleManagementModule'},
-            { path: 'order-management', loadChildren: '../modules/order-management/order-management.module#OrderManagementModule'},
-            { path: 'kpi-dashboard', loadChildren: '../modules/kpi-dashboard/kpi-dashboard.module#KpiDashboardModule'},
-            { path: 'reconfiguration-overview', loadChildren: '../modules/reconfiguration-overview/reconfiguration-overview.module#ReconfigurationOverviewModule'},
-            { path: 'ops-configuration', loadChildren: '../modules/ops-configuration/ops-configuration.module#OpsConfigurationModule'},
-            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'module-management', loadChildren: () => import('../modules/module-management/module-management.module').then(m => m.ModuleManagementModule)},
+            { path: 'order-management', loadChildren: () => import('../modules/order-management/order-management.module').then(m => m.OrderManagementModule)},
+            { path: 'kpi-dashboard', loadChildren: () => import('../modules/kpi-dashboard/kpi-dashboard.module').then(m => m.KpiDashboardModule)},
+            { path: 'reconfiguration-overview', loadChildren: () => import('../modules/reconfiguration-overview/reconfiguration-overview.module').then(m => m.ReconfigurationOverviewModule)},
+            { path: 'ops-configuration', loadChildren: () => import('../modules/ops-configuration/ops-configuration.module').then(m => m.OpsConfigurationModule)},
+            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+            { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule)},
+            { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule)},
         ]
     }
 ];
+
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
