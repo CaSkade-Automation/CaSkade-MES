ba7482309a6dc64f796a6c0e15b24ee817c6d63a
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-06-11 15:30:39 +0200

    Started refactoring the module component

--- frontend/src/modules/module-management/module-management.component.ts
+++ frontend/src/modules/module-management/module-management.component.ts
@@ -1,114 +1,106 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from "../../shared/services/socket.service";
 import { HttpClient } from '@angular/common/http';
-import { ManufacturingModule, ServiceExecutionDescription, Method, SelectedParameter } from '../../shared/models/self-description';
+import { ServiceExecutionDescription, Method, SelectedParameter } from '../../shared/models/self-description';
 import { ModuleService } from '../../shared/services/module.service';
 import { ManufacturingServiceExecutor } from './manufacturing-service-executor.service';
+import { ProductionModule } from '../../../../shared/models/production-module/ProductionModule';
+import { take } from 'rxjs/operators';
 
 @Component({
-  selector: 'module-management',
-  templateUrl: 'module-management.component.html',
+    selector: 'module-management',
+    templateUrl: 'module-management.component.html',
 })
 export class ModuleManagementComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient,
-    private socketService: SocketService,
-    private moduleService: ModuleService,
-    private mfgServiceExecutor: ManufacturingServiceExecutor) {}
+    constructor(private httpClient: HttpClient,
+        private socketService: SocketService,
+        private moduleService: ModuleService,
+        private mfgServiceExecutor: ManufacturingServiceExecutor) { }
 
-  incomingmsg = [];
-  modules = new Array<ManufacturingModule>();
-  serviceExecutionDescription: ServiceExecutionDescription;
-  parameterValues = new Array<string>();
+    incomingmsg = [];
+    modules = new Array<ProductionModule>();
+    serviceExecutionDescription: ServiceExecutionDescription;
+    parameterValues = new Array<string>();
 
-  ngOnInit() {
-    this.moduleService.getAllModules().subscribe(manufacturingModules => {
-      this.modules = manufacturingModules;
-      this.modules.forEach(manufacturingModule => {
-        this.moduleService.getAllCapabilitiesOfModule(manufacturingModule.name).subscribe(moduleProcesses => {
-          manufacturingModule.addProcesses(moduleProcesses);
+    ngOnInit() {
+        console.log("init");
+
+        this.moduleService.getAllModulesWithCapabilities().subscribe((modules: ProductionModule[]) => {
+            this.modules = modules;
         });
-      });
+        // this.moduleService.getAllModules().subscribe(productionModules => {
+        //     console.log(productionModules);
 
-    });
+        //     this.modules = productionModules;
+        //     this.modules.forEach(manufacturingModule => {
+        //         this.moduleService.getAllCapabilitiesOfModule(manufacturingModule.iri).subscribe(moduleProcesses => {
+        //             manufacturingModule.addCapabilities(moduleProcesses);
+        //         });
+        //     });
 
-    this.socketService.getMessage().subscribe(msg => {
-      this.moduleService.getAllModules().subscribe(data => {
-        const currentModules: Array<ManufacturingModule> = data;
-        this.addNewModules(currentModules);
-        this.modules.forEach(manufacturingModule => {
-          this.moduleService.getAllCapabilitiesOfModule(manufacturingModule.name).subscribe(moduleProcesses => {
-            manufacturingModule.addProcesses(moduleProcesses);
-          });
+        // });
+
+        this.socketService.getMessage().subscribe(msg => {
+            this.moduleService.getAllModules().subscribe(data => {
+                const currentModules: Array<ProductionModule> = data;
+                this.modules = currentModules;
+                this.modules.forEach(manufacturingModule => {
+                    this.moduleService.getAllCapabilitiesOfModule(manufacturingModule.iri).subscribe(moduleProcesses => {
+                        manufacturingModule.addCapabilities(moduleProcesses);
+                    });
+                });
+            });
         });
-      });
-    });
-  }
-
-  sendMsg(msg) {
-    this.socketService.sendMessage(msg);
- }
-
-
-  executeModuleService(method: Method) {
-    let selectedParams = new Array<SelectedParameter>();
-
-    // Create the selected parameters
-    for (let i = 0; i < method.parameters.length; i++) {
-      const param = method.parameters[i];
-      const paramValue = this.parameterValues[i];
-
-      selectedParams.push(new SelectedParameter(param, paramValue));
     }
-    let executionDescription = new ServiceExecutionDescription(method.getFullPath(), method.getShortMethodType(), selectedParams);
-    this.mfgServiceExecutor.executeService(executionDescription)
-  }
+
+    sendMsg(msg) {
+        this.socketService.sendMessage(msg);
+    }
 
 
-  /**
-   * Compares the existing modules-array with a new new list of all currently connected modules, finds out which modules are new and adds them to the modules-array
-   * @param newModules Array of all modules that are currently connected
-   */
-  addNewModules(newModules: Array<ManufacturingModule>): ManufacturingModule{
-    // Get only the modules that are not already displayed
-    if(this.modules.length != newModules.length) {
-      this.modules.forEach(module => {
-        for (let i = 0; i < newModules.length; i++) {
-          const newModule = newModules[i];
-          if(module.equals(newModule)) {
-            newModules.splice(i, 1);
-          }
+    executeModuleService(method: Method) {
+        let selectedParams = new Array<SelectedParameter>();
+
+        // Create the selected parameters
+        for (let i = 0; i < method.parameters.length; i++) {
+            const param = method.parameters[i];
+            const paramValue = this.parameterValues[i];
+
+            selectedParams.push(new SelectedParameter(param, paramValue));
         }
-      });
-
-    // Add the new modules
-    newModules.forEach(newModule => {
-      this.modules.push(newModule);
-    });
-    } else{
-      // same length -> no new module
-      return null;
+        let executionDescription = new ServiceExecutionDescription(method.getFullPath(), method.getShortMethodType(), selectedParams);
+        this.mfgServiceExecutor.executeService(executionDescription)
     }
-  }
+
+
+    /**
+     * Compares the existing modules-array with a new new list of all currently connected modules, finds out which modules are new and adds them to the modules-array
+     * @param newModule Array of all modules that are currently connected
+     */
+    addNewModules(newModule: ProductionModule): void {
+        // Add the new module
+        this.modules.push(newModule);
+    }
 
 
 
 
-  // disconnectModule(moduleId) {
-  //   // TODO: Post to API to really delete element
-  //   this.httpClient.delete(`api/modules/${moduleId}`).subscribe(
-  //     data => {this.removeModuleCard(moduleId)}),
-  //     error => console.log('An error happened, module could not be disconnected'
-  //   );
-  // }
+    disconnectModule(moduleIri) {
+      // TODO: Post to API to really delete element
+      this.httpClient.delete(`api/modules/${moduleIri}`).subscribe(
+        data => {this.removeModuleCard(moduleIri)}),
+        error => console.log('An error happened, module could not be disconnected'
+      );
+    }
 
-  // removeModuleCard(moduleId) {
-  //   // remove module with that id from the list
-  //   for (let i = 0; i < this.modules.length; i++) {
-  //     if (this.modules[i].name == moduleId) {
-  //       this.modules.splice(i, 1);
-  //       break;
-  //     }
-  //   }
-  // }
+    removeModuleCard(moduleIri) {
+      // remove module with that id from the list
+      for (let i = 0; i < this.modules.length; i++) {
+        if (this.modules[i].iri == moduleIri) {
+          this.modules.splice(i, 1);
+          break;
+        }
+      }
+    }
 }
