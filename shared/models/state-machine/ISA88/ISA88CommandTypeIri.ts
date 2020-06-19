babdcaa1d7e1432ebfe34d177883916605d698b6
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-06-19 09:39:55 +0200

    Just renamed

---
+++ shared/models/state-machine/ISA88/ISA88CommandTypeIri.ts
@@ -0,0 +1,11 @@
+export enum Isa88CommandTypeIri {
+    Abort = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Abort_Command",
+    Clear = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Clear_Command",
+    Hold = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Hold_Command",
+    Reset = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Reset_Command",
+    Start = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Start_Command",
+    Stop = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Stop_Command",
+    Suspend = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Suspend_Command",
+    Unhold = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Un-Hold_Command",
+    Unsuspend = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Unsuspend_Command"
+}
---
+++ shared/models/state-machine/ISA88/ISA88StateTypeIri.ts
@@ -0,0 +1,20 @@
+export enum Isa88StateTypeIri {
+    Aborting = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Aborting",
+    Clearing = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Clearing",
+    Complete = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Complete",
+    Completing = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Completing",
+    Execute = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Execute",
+    Holding = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Holding",
+    Resetting = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Resetting",
+    Starting = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Starting",
+    Stopping = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Stopping",
+    Suspending = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Suspending",
+    Unholding = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Unholding",
+    Unsuspending = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Unsuspending",
+
+    Aborted = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Aborted",
+    Held = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Held",
+    Idle = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle",
+    Stopped = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Stopped",
+    Suspended = "http://www.hsu-ifa.de/ontologies/ISA-TR88#Suspended"
+}
