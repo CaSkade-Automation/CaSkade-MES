7c2764edd074a5ea81f6bdddd3371cdb9dc7eaab
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-07-29 21:06:00 +0200

    Added details to RestSkill query

--- backend/routes/skill-execution/executors/RestSkillExecutor.ts
+++ backend/routes/skill-execution/executors/RestSkillExecutor.ts
@@ -1,26 +1,34 @@
-import { SkillExecutor } from '../SkillExecutor';
+import { SkillExecutor } from './SkillExecutor';
 import { SkillExecutionRequestDto } from '@shared/models/skill/SkillExecutionRequest';
 import axios, { AxiosRequestConfig } from 'axios';
-import { SkillParameter } from '../../../../shared/models/skill/SkillParameter';
+import { SkillParameter, SkillParameterDto } from '@shared/models/skill/SkillParameter';
 import { GraphDbConnectionService } from 'util/GraphDbConnection.service';
+import { SparqlResultConverter } from 'sparql-result-converter';
+import { opcUaSkillExecutionMapping } from './skill-execution-mappings';
 
 export class RestSkillExecutionService implements SkillExecutor {
 
 
-    constructor(private graphDbConnection: GraphDbConnectionService) {}
+    constructor(
+        private graphDbConnection: GraphDbConnectionService,
+        private converter = new SparqlResultConverter()) {}
+
+    setSkillParameters(parameters: SkillParameterDto[]): void {
+        throw new Error("Method not implemented.");
+    }
 
     executeSkill(executionRequest: SkillExecutionRequestDto): void {
-        // // throw new Error("Method not implemented.");
+        // throw new Error("Method not implemented.");
 
-        // // // get the full REST service description
-        // const serviceDescription = this.getRestServiceDescription();
+        // // get the full REST service description
+        const serviceDescription = this.getRestServiceDescription(executionRequest.skillIri, executionRequest.commandTypeIri);
 
-        // // const queryParams = [];
-        // // serviceDescription.parameters.filter(parameter => {
-        // //     if((parameter.location == "other") && (parameter.type == "QueryParameter")) {
-        // //         queryParams.push(parameter);
-        // //     }
-        // // });
+        // const queryParams = [];
+        // serviceDescription.parameters.filter(parameter => {
+        //     if((parameter.location == "other") && (parameter.type == "QueryParameter")) {
+        //         queryParams.push(parameter);
+        //     }
+        // });
 
 
         // // Create a simple json object for all body parameters
@@ -58,21 +66,35 @@ export class RestSkillExecutionService implements SkillExecutor {
     }
 
 
-    private getRestServiceDescription(skillIri: string, commandIri: string): RestServiceDescription {
+    private async getRestServiceDescription(skillIri: string, commandIri: string): Promise<RestSkillDescription> {
         const query = `
-            SELECT ?httpMethod ?path ?basePath ?parameters WHERE {
-                <${skillIri}> a Cap:RestSkill.
-                <${commandIri}> a ISA88:Transition;
-                    Cap:invokedBy ?skillMethod.
-                ?skillMethod sesame:directSubClassOf WADL:Method.
+        SELECT ?path ?basePath ?httpMethod ?parameters WHERE {
+            <${skillIri}> a Cap:RestSkill;
+                WADL:hasBase ?basePath;
+                WADL:hasResource ?resource.
+            ?resource WADL:hasPath ?path;
+                WADHL:hasMethod ?skillMethod.
+            ?skillMethod rdf:type ?httpMethod.
+            ?httpMethod sesame:directSubClassOf WADL:Method.
+            <${commandIri}> a ISA88:Transition;
+                Cap:invokedBy ?skillMethod.
+            OPTIONAL {
+                ?skillMethod WADL:hasRequest/WADL:hasParameter ?param.
+                ?param WADL:hasParameterName ?paramName;
+                    WADL:hasParameterType ?paramType;
+                    WADL:hasParameterDefault ?default;
+                    WADL:hasParameterRequired ?required;
+                ?
             }
-		`;
+        }`;
+        const queryResult = await this.graphDbConnection.executeQuery(query);
+        const mappedResult = this.converter.convert(queryResult.results.bindings, opcUaSkillExecutionMapping);
         return null;
     }
 
 }
 
-interface RestServiceDescription {
+interface RestSkillDescription {
    httpMethod: string;
    fullPath: string;
    parameters: SkillParameter[];
