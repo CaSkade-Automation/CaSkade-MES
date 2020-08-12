549f2854fdf82f9846acf7098e39bac843828922
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-12 10:02:42 +0200

    Added container component for properties together with two possible sub components for different bpmn elements

---
+++ frontend/src/modules/skill-processes/bpmn-component/properties-panel/properties-subcomponents/flow-properties/flow-properties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-flow-properties',
+    templateUrl: './flow-properties.component.html',
+    styleUrls: ['./flow-properties.component.scss']
+})
+export class FlowPropertiesComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
---
+++ frontend/src/modules/skill-processes/bpmn-component/properties-panel/properties-subcomponents/properties/properties.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-properties',
+    templateUrl: './properties.component.html',
+    styleUrls: ['./properties.component.scss']
+})
+export class PropertiesComponent {
+
+    constructor() {}
+
+}
