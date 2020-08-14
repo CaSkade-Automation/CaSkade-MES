b5b0135ac7ab01fd5f47f797288241188df2258f
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-14 11:19:40 +0200

    Cleanup and minor style changes

--- frontend/src/modules/skill-processes/skill-processes.module.ts
+++ frontend/src/modules/skill-processes/skill-processes.module.ts
@@ -4,16 +4,21 @@ import { SkillProcessesComponent } from './skill-processes.component';
 import { SkillProcessesRoutes } from './skill-processes.routing';
 import { BpmnDiagramComponent } from './bpmn-component/bpmn-diagram.component';
 import { PropertiesPanelComponent } from './bpmn-component/properties-panel/properties-panel.component';
+import { PropertyController } from './bpmn-component/properties-panel/properties-subcomponents/property-controller/PropertyController';
+import { DynamicPropertyComponent } from './bpmn-component/properties-panel/properties-subcomponents/dynamic-property/dynamic-property.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
-        SkillProcessesRoutes
+        SkillProcessesRoutes,
+        ReactiveFormsModule
     ],
     declarations: [
         SkillProcessesComponent,
         BpmnDiagramComponent,
-        PropertiesPanelComponent
-    ]
+        PropertiesPanelComponent,
+        DynamicPropertyComponent
+    ],
 })
 export class SkillProcessesModule { }
