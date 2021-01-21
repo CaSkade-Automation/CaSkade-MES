211def40f9d37ac60a567ff1c3ec7dbb1c8357af
Tom Jeleniewski <jeleniewskitom@gmail.com>
2021-01-21 13:43:40 +0100

    Added function to delete bpmn process-instances and to get XML file of a process-definition

--- frontend/src/modules/skill-processes/process-control/process-control.component.ts
+++ frontend/src/modules/skill-processes/process-control/process-control.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ProcessDefinition } from '@shared/models/processDefinition/ProcessDefinition';
 import { ProcessInstance } from '@shared/models/ProcessInstance/ProcessInstance';
@@ -16,6 +16,9 @@ export class ProcessControlComponent implements OnInit {
     processInstances= Array<ProcessInstance>();
     selectedDefinition: ProcessDefinition;
     instanceOperate: ProcessInstance;
+    jsn: string;
+    bpmnXml: string;
+    processDefinitionIdOfXml: string;
    
 
     ngOnInit(): void {
@@ -39,7 +42,18 @@ export class ProcessControlComponent implements OnInit {
     }
        
     deleteProcessInstance(processInstance: ProcessInstance): void{
-        this.processControlService.deleteProcessInstance(processInstance).subscribe();
+        this.processControlService.deleteProcessInstance(processInstance).subscribe((processInstances: ProcessInstance[])=>{
+            this.processInstances=processInstances;
+        });
+    }
+    getXmlOfProcessDefinition(processDefinition: ProcessDefinition):  string{
+      
+        return this.processControlService.getXMLofProcessDefinition(processDefinition).subscribe(data=>{
+            this.bpmnXml= data.bpmn20Xml;
+            this.processDefinitionIdOfXml= data.id;
+            console.log(this.bpmnXml);
+        });
+
     } 
 
 }
--- frontend/src/modules/skill-processes/process-control/process-control.service.ts
+++ frontend/src/modules/skill-processes/process-control/process-control.service.ts
@@ -11,6 +11,7 @@ import { map } from 'rxjs/operators';
 })
 export class ProcessControlService {
   engineRestRoot= "/engine-rest";
+  xml: string;
   
   //observer: Observer<ProcessDefinitionDto[]>
   constructor(private  http: HttpClient) {}
@@ -39,6 +40,7 @@ export class ProcessControlService {
               data.diagram, data.suspended, data.tenantId, data.versionTag,
               data.historyTimeToLive, data.startableInTasklist))
       );
+
   }
 
   getAllProcessInstances(): Observable<ProcessInstance[]>{
@@ -56,10 +58,25 @@ export class ProcessControlService {
   }
   deleteProcessInstance(processInstance: ProcessInstance): Observable<ProcessInstance[]> {
       const URL= `${this.engineRestRoot}/process-instance/${processInstance.id}`;
-      this.http.delete(URL);
+      console.log(URL);
+    
+      this.http.delete(URL).subscribe();
+
       return this.getAllProcessInstances();
       
   }
+  getXMLofProcessDefinition(processDefinition: ProcessDefinition): any{
+      const URL= `${this.engineRestRoot}/process-definition/${processDefinition.id}/xml`; 
+      
+  
+  
+      return this.http.get<any>(URL);
+      //   .subscribe(data=>{
+      //       this.xml=data.bpmn20Xml;
+      //console.log(this.xml);
+      // });
+  }
+ 
  
 
 }
