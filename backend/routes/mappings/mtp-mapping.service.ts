5c26dba1dc2f12a5b624ebd225b2154068e46023
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-03-07 16:11:16 +0100

    Now returning config object

--- backend/routes/mappings/mtp-mapping.service.ts
+++ backend/routes/mappings/mtp-mapping.service.ts
@@ -1,29 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import Axios, { AxiosRequestConfig } from 'axios';
+import {MtpMappingServiceConfig} from '@shared/models/mappings/MtpMappingServiceConfig';
 
 @Injectable()
 export class MtpMappingService {
 
-    private url: string;
+    private config: MtpMappingServiceConfig = {
+        url: "http://localhost:9191"
+    }
+
 
     constructor() {
-        this.url = "http://localhost:9191";
     }
 
     /**
-     * Completely exchanges the host configuration against a new one
-     * @param {*} newConfig The new host config (consisting of host, user, pw)
+     * Set a new URL
+     * @param {*} newUrl The new URL of the mapping service
      */
-    async setUrl(newUrl: string): Promise<string> {
-        this.url = newUrl;
-        return this.url;
+    setUrl(newUrl: string): MtpMappingServiceConfig {
+        this.config.url = newUrl;
+        return this.config;
     }
 
     /**
      * Returns the current URL
      */
-    getUrl(): string {
-        return this.url;
+    getConfig(): MtpMappingServiceConfig{
+        return this.config;
     }
 
     /**
@@ -31,10 +34,10 @@ export class MtpMappingService {
      * @param mtpFile File containing an MTP
      */
     async executeMapping(mtpFile: Express.Multer.File): Promise<string> {
-        const config: AxiosRequestConfig = {
+        const reqConfig: AxiosRequestConfig = {
             headers: {'Content-Type': 'multipart/form-data'}
         };
-        return Axios.post(this.url, mtpFile, config);
+        return Axios.post(this.config.url, mtpFile, reqConfig);
     }
 
 }
