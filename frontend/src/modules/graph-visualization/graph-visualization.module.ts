97d575242db37bf47b6af6b0b95a9817f07fa051
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-05-14 15:43:20 +0200

    First step to fixing the graph visualization

--- frontend/src/modules/graph-visualization/graph-visualization.module.ts
+++ frontend/src/modules/graph-visualization/graph-visualization.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { GraphVisualizationComponent } from './graph-visualization.component';
 import {GraphVisualizationRouter} from './graph-visualization.routing';
-import { ModuleService } from 'app/shared/services/module.service';
+import { ModuleService } from '../../shared/services/module.service';
 import { NodeCreatorService } from './node-creator.service';
 
 @NgModule({
-  imports: [
-    GraphVisualizationRouter,
-  ],
-  declarations: [GraphVisualizationComponent],
-  providers: [
-    ModuleService,
-    NodeCreatorService
-    
-  ]
+    imports: [
+        GraphVisualizationRouter,
+    ],
+    declarations: [GraphVisualizationComponent],
+    providers: [
+        ModuleService,
+        NodeCreatorService
+
+    ]
 })
 export class GraphVisualizationModule { }
--- frontend/src/modules/graph-visualization/graph-visualization.routing.ts
+++ frontend/src/modules/graph-visualization/graph-visualization.routing.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 import {GraphVisualizationComponent} from './graph-visualization.component';
 
 const routes: Routes = [
-  { path: 'modules/:moduleName',
-  component: GraphVisualizationComponent,
-  
-   },
-   {path:'modules', component: GraphVisualizationComponent
+    { path: 'modules/:moduleName',
+        component: GraphVisualizationComponent,
 
-   }
+    },
+    {path:'modules', component: GraphVisualizationComponent
+
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 
 export class GraphVisualizationRouter {}
