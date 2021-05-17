ec5e8c95bd2d0781e8213472224ab0394024b222
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-05-17 11:32:52 +0200

    Added capability and skill graph visus

--- frontend/src/modules/graph-visualization/D3GraphData.ts
+++ frontend/src/modules/graph-visualization/D3GraphData.ts
@@ -1,7 +1,46 @@
 
+
+export enum NodeType {
+    None = "None",
+    D3ModuleNode ="D3ModuleNode",
+    D3SkillNode = "D3SkillNode",
+    D3CapabilityNode = "D3CapabilityNode"
+}
+
+export class D3Node {
+    constructor(
+        public id: string,
+        public name: string,
+        public group: number,
+        public type: NodeType = NodeType.None) { }
+}
+
+export class D3ModuleNode extends D3Node {
+    constructor(id: string, name: string) {
+        super(id, name, 1, NodeType.D3ModuleNode);
+    }
+}
+export class D3SkillNode extends D3Node{
+    constructor(id: string, name: string) {
+        super(id, name, 100, NodeType.D3SkillNode);
+    }
+}
+
+export class D3CapabilityNode extends D3Node{
+    constructor(id: string, name: string) {
+        super(id, name, 200, NodeType.D3CapabilityNode);
+    }
+}
+
+export class D3Link {
+    constructor(
+        public source: string,
+        public target: string,
+        public type: string) { }
+}
+
+
 export class D3GraphData {
-    // nodes: D3Node [] = [];
-    // links: D3Link[] = [];
     constructor(private nodes: D3Node[] = [], private links: D3Link[] = []) { }
 
     addNode(node: D3Node): void {
@@ -11,19 +50,33 @@ export class D3GraphData {
     addLink(link: D3Link): void {
         this.links.push(link);
     }
-}
 
+    getNodes(): D3Node[] {return this.nodes;}
+    getNodesById(id: string): D3Node[] {
+        return this.nodes.filter(node => node.id == id);
+    }
 
-export class D3Node {
-    constructor(
-        private id: string,
-        private name: string,
-        private group: number) { }
-}
+    getNodesByNodeTyp(type: string): D3Node[] {
+        return this.nodes.filter(node => node.type === type);
+    }
 
-export class D3Link {
-    constructor(
-        private source: string,
-        private target: string,
-        private type: string) { }
+    getLinks(): D3Link[] {return this.links;}
+
+    // Adds another D3GraphData object to the current one
+    appendAndConnectData(data: D3GraphData, idToConnectTo: string, nodeType: NodeType, connectionType: string): void {
+        const newNodes = data.getNodes();
+        const newLinks = data.getLinks();
+        // Add the new data
+        newNodes.forEach(newNode => this.addNode(newNode));
+        newLinks.forEach(newLink => this.addLink(newLink));
+        // Connect with existing nodes
+        const oldNodesToConnect = this.getNodesById(idToConnectTo);
+        const newNodesToConnect = this.getNodesByNodeTyp(nodeType);
+
+        oldNodesToConnect.forEach(oldNode => {
+            newNodesToConnect.forEach(newNode => {
+                this.addLink(new D3Link(oldNode.id, newNode.id, connectionType));
+            });
+        });
+    }
 }
