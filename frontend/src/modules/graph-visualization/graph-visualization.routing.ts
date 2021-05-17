4c73b8fc31c2f8bbc43a2b6a5ce349afad8412b3
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-05-17 11:32:52 +0200

    Added capability and skill graph visus

--- frontend/src/modules/graph-visualization/graph-visualization.routing.ts
+++ frontend/src/modules/graph-visualization/graph-visualization.routing.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {GraphVisualizationComponent} from './graph-visualization.component';
 
 const routes: Routes = [
-    { path: 'modules/:moduleName',
+    { path: ':elementType/:elementIri',
         component: GraphVisualizationComponent,
 
     },
