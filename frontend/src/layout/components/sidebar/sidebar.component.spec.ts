ae428c4e4b448e412f8804b2101ecee794c43211
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-05-31 11:26:35 +0200

    Update Angular to v11

--- frontend/src/layout/components/sidebar/sidebar.component.spec.ts
+++ frontend/src/layout/components/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -6,27 +6,27 @@ import { SidebarComponent } from './sidebar.component';
 import { LayoutModule } from '../../layout.module';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+    let component: SidebarComponent;
+    let fixture: ComponentFixture<SidebarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        LayoutModule,
-        RouterTestingModule,
-        TranslateModule.forRoot(),
-      ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                LayoutModule,
+                RouterTestingModule,
+                TranslateModule.forRoot(),
+            ],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SidebarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/src/shared/guard/auth.guard.spec.ts
+++ frontend/src/shared/guard/auth.guard.spec.ts
@@ -1,17 +1,17 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
-      providers: [AuthGuard]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ RouterTestingModule ],
+            providers: [AuthGuard]
+        });
     });
-  });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+    it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
