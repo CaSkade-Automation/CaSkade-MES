f926771eac71426a248aba0e4701ef97e360fa7d
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-07-26 15:15:09 +0200

    Simplified constructor / attributes

--- shared/models/processInstance/ProcessInstance.ts
+++ shared/models/processInstance/ProcessInstance.ts
@@ -7,7 +7,7 @@ export class ProcessInstanceDto {
         ended: boolean;
         suspended: boolean;
         tenantId: string;
-       
+
         constructor( links: Array<string>,
             id: string,
             definitionId: string,
@@ -27,30 +27,12 @@ export class ProcessInstanceDto {
         }
 }
 export class ProcessInstance {
-        links: Array<string>;
-        id: string;
-        definitionId: string;
-        businessKey: string;
-        caseInstanceId: string;
-        ended: boolean;
-        suspended: boolean;
-        tenantId: string;
-       
-        constructor( links: Array<string>,
-            id: string,
-            definitionId: string,
-            businessKey: string,
-            caseInstanceId: string,
-            ended: boolean,
-            suspended: boolean,
-            tenantId: string){
-           this.links=links;
-           this.id=id;
-           this.definitionId=definitionId;
-           this.businessKey=businessKey;
-           this.caseInstanceId=caseInstanceId;
-           this.ended=ended;
-           this.suspended=suspended;
-           this.tenantId=tenantId;
-        }
+        constructor(public links: Array<string>,
+            public id: string,
+            public definitionId: string,
+            public businessKey: string,
+            public caseInstanceId: string,
+            public ended: boolean,
+            public suspended: boolean,
+            public tenantId: string) {}
 }
