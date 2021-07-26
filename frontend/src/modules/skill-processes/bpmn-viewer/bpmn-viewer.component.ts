f207bbc834e24d1dbf664e611e892df411d96166
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-07-26 09:53:06 +0200

    Added little bpmn viewer component without control

---
+++ frontend/src/modules/skill-processes/bpmn-viewer/bpmn-viewer.component.ts
@@ -0,0 +1,34 @@
+import { AfterContentInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import * as BpmnViewer from 'bpmn-js/dist/bpmn-viewer.production.min.js';
+
+@Component({
+    selector: 'bpmn-viewer',
+    templateUrl: './bpmn-viewer.component.html',
+    styleUrls: ['./bpmn-viewer.component.scss']
+})
+export class BpmnViewerComponent implements AfterContentInit, OnDestroy {
+    private bpmnViewer: BpmnViewer;
+
+    @ViewChild('ref', { static: true }) private el: ElementRef;
+
+    @Input() set bpmnXml(bpmnXml: string) {
+        if (bpmnXml) {
+            this.bpmnViewer.importXML(bpmnXml);
+        }
+    }
+
+    constructor() {
+        this.bpmnViewer = new BpmnViewer();
+        this.bpmnViewer.on('import.done', () => {
+            this.bpmnViewer.get('canvas').zoom('fit-viewport');
+        });
+    }
+
+    ngAfterContentInit(): void {
+        this.bpmnViewer.attachTo(this.el.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this.bpmnViewer.destroy();
+    }
+}
