610ae36f67eb484dcf0843878cf76d07ca109fe9
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-07-27 14:07:13 +0200

    Completed initial version of instance service and component

---
+++ frontend/src/shared/services/process-instance.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { ProcessInstanceService } from './process-instance.service';
+
+describe('Service: ProcessInstance', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ProcessInstanceService]
+    });
+  });
+
+  it('should ...', inject([ProcessInstanceService], (service: ProcessInstanceService) => {
+    expect(service).toBeTruthy();
+  }));
+});
