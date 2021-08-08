79443fb062158b5bb084305a484e0de9f545a1c4
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-08-08 18:11:32 +0200

    Clean-up and comments

--- frontend/src/modules/skill-processes/bpmn-diagram/bpmn-modeler.component.ts
+++ frontend/src/modules/skill-processes/bpmn-diagram/bpmn-modeler.component.ts
@@ -8,7 +8,8 @@ import { AfterContentInit, Component, ElementRef, Input, OnDestroy, Output, View
  * bpmn-modeler - bootstraps a full-fledged BPMN editor
  */
 import * as BpmnModeler from 'bpmn-js/dist/bpmn-modeler.production.min.js';
-import { Observable } from 'rxjs';
+import * as propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+import * as camundaModdleDescriptor from  'camunda-bpmn-moddle';
 import { BpmnDataModel } from './BpmnDataModel';
 import { emptyXml } from './emptyDiagram';
 
@@ -18,11 +19,11 @@ import { emptyXml } from './emptyDiagram';
     styleUrls: ['./bpmn-modeler.component.scss']
 })
 export class BpmnDiagramComponent implements AfterContentInit, OnDestroy {
-    private bpmnModeler: BpmnModeler;
-    private bpmnDataModel: BpmnDataModel;
-    private elementRegistry;
 
-    @ViewChild('ref', { static: true }) private el: ElementRef;
+    public bpmnModeler: BpmnModeler;       // bpmn-js modeler that is passed to the properties panel
+    public clickedElement: any;            // Clicked BPMN element that is passed to the properties panel
+
+    @ViewChild('ref', { static: true }) private el: ElementRef;     // Reference to the DOM element
 
     @Input() set bpmnXml(bpmnXml: string) {
         if (bpmnXml) {
@@ -30,51 +31,63 @@ export class BpmnDiagramComponent implements AfterContentInit, OnDestroy {
         }
     }
 
-    clickedElement: any;
-    showPropertiesPanel: boolean;
+
 
     constructor() {
-        this.bpmnModeler = new BpmnModeler();
-        this.bpmnDataModel = new BpmnDataModel(this.bpmnModeler.get('modeling'));
-        this.elementRegistry = this.bpmnModeler.get('elementRegistry');
+        // setup the modler
+        this.bpmnModeler = new BpmnModeler({
+            additionalModules: [propertiesProviderModule],
+            moddleExtensions: {
+                camunda: camundaModdleDescriptor
+            }
+        });
 
-        this.bpmnModeler.importXML(emptyXml);
+        this.bpmnModeler.importXML(emptyXml).then(() => {
+            // When done importing, get the process element and set it as the initial clicked element
+            const process = this.bpmnModeler.get('canvas').getRootElement();
+            this.clickedElement = process;
+        });
 
-
-
-        console.log(this.elementRegistry);
-        console.log(this.bpmnDataModel);
-
-        // this.dataModel.updateProperties()
-
-        // this.bpmnModeler.on('import.done', ({ error }) => {
-        //     if (!error) {
-        //         // this.bpmnModeler.get('canvas').zoom('fit-viewport');
-        //     }
-        // });
+        // Setup listener for later clicks
         this.bpmnModeler.on('element.click', (event) => this.onDiagramElementClicked(event));
     }
 
-    clear() {
-        this.bpmnModeler.importXML(emptyXml);
-    }
 
-    saveXml(): Promise<{xml: string}> {
-        return this.bpmnModeler.saveXML({ format: true },);
-    }
-
+    /**
+     * Bind the modeler to the DOM element as soon as the DOM is rendered
+     */
     ngAfterContentInit(): void {
         this.bpmnModeler.attachTo(this.el.nativeElement);
     }
 
+    /**
+     * Free the DOM element and kill all listeners / BPMN services
+     */
     ngOnDestroy(): void {
         this.bpmnModeler.destroy();
     }
 
-
-    onDiagramElementClicked(event): void {
-        this.clickedElement = event.element;
+    /**
+     * Reset the modeler back to the initial process only containing a start event
+     */
+    clear(): void {
+        this.bpmnModeler.importXML(emptyXml);
     }
 
+    /**
+     * Get the current model content in BPMN XML
+     * @returns The current model state serialized in BPMN XML
+     */
+    saveXml(): Promise<{xml: string}> {
+        return this.bpmnModeler.saveXML({ format: true },);
+    }
+
+    /**
+     * Gets executed on every click on the modeler. Changes the selected element so that the properties panel changes
+     * @param event Click event containing the clicked BPMN element
+     */
+    onDiagramElementClicked(event: any): void {
+        this.clickedElement = event.element;
+    }
 
 }
