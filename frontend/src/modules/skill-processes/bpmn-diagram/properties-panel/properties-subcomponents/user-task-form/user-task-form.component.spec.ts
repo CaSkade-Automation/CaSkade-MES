96caba55819b2809e874e54d8ec213901c6e0137
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-11-07 18:19:09 +0100

    Added user task form with the new code approach

---
+++ frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/user-task-form/user-task-form.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { UserTaskFormComponent } from './user-task-form.component';
+
+describe('UserTaskFormComponent', () => {
+  let component: UserTaskFormComponent;
+  let fixture: ComponentFixture<UserTaskFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserTaskFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserTaskFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
