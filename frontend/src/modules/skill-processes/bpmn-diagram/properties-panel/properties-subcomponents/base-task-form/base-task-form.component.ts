3b5459af9d28842e07216b19d0f5c79d1a6ad74a
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-11-07 16:51:45 +0100

    Completely changed approach to implement custom property panel

---
+++ frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/base-task-form/base-task-form.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs/operators';
+import { BpmnDataModel, BpmnProperty } from '../../../BpmnDataModel';
+
+@Component({
+    selector: 'base-task-form',
+    templateUrl: './base-task-form.component.html',
+    styleUrls: ['./base-task-form.component.scss']
+})
+export class BaseTaskFormComponent implements OnInit, OnChanges {
+
+    @Input() bpmnElement;
+    @Input() dataModel: BpmnDataModel;
+    @Output() basePropertyUpdated = new EventEmitter<BpmnProperty>();
+
+    fg: FormGroup;
+
+    constructor() { }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.fg?.patchValue({
+            id: this.bpmnElement?.id,
+            type: this.bpmnElement?.type,
+            name: this.bpmnElement?.businessObject.name
+        });
+    }
+
+    ngOnInit() {
+        this.fg = new FormGroup({
+            id: new FormControl(this.bpmnElement?.id),
+            type: new FormControl({value: this.bpmnElement?.type, disabled: true}),
+            name: new FormControl(this.bpmnElement?.businessObject.name)
+        });
+        this.fg.valueChanges.pipe(debounceTime(100)).subscribe(change => {
+            Object.keys(change).forEach(changeKey => {
+                const prop = new BpmnProperty(changeKey, change[changeKey]);
+                this.basePropertyUpdated.emit(prop);
+            });
+        });
+    }
+
+}
