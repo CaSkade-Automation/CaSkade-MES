2e6d9809070f936782e3a99a34a7990c5c2e645f
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-11-07 19:20:12 +0100

    Fixed minor problems with getting assigned user

--- frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/user-task-form/user-task-form.component.ts
+++ frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/user-task-form/user-task-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { BpmnProperty } from '../../../BpmnDataModel';
@@ -8,7 +8,7 @@ import { BpmnProperty } from '../../../BpmnDataModel';
     templateUrl: './user-task-form.component.html',
     styleUrls: ['./user-task-form.component.scss']
 })
-export class UserTaskFormComponent implements OnInit {
+export class UserTaskFormComponent implements OnInit, OnChanges {
 
     @Input() bpmnElement;
     @Output() basePropertyUpdated = new EventEmitter<BpmnProperty>();
@@ -17,17 +17,22 @@ export class UserTaskFormComponent implements OnInit {
 
     ngOnInit(): void {
         this.fg = new FormGroup({
-            assignee: new FormControl(this.bpmnElement?.assignee),
+            assignee: new FormControl(this.bpmnElement?.businessObject.assignee),
         });
 
-        this.assignee.valueChanges.subscribe(assignee => {
-            const prop = new BpmnProperty("camunda:assignee", assignee);
+        this.assignee?.valueChanges.subscribe(assignee => {
+            const prop = new BpmnProperty("assignee", assignee);
             this.basePropertyUpdated.emit(prop);
         });
     }
 
+    // OnChanges is needed to change form values when another UserTask is clicked as this doesn't trigger OnInit
+    ngOnChanges(changes: SimpleChanges): void {
+        this.assignee?.setValue(this.bpmnElement.businessObject.assignee);
+    }
+
     get assignee(): FormControl {
-        return this.fg.controls.assignee as FormControl;
+        return this.fg?.controls.assignee as FormControl;
     }
 
 }
