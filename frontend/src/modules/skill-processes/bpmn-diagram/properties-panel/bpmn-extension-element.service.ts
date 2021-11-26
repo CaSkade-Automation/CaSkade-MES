87a81f4d960299de5f975c6f178bcfdda0614a26
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-11-26 14:00:25 +0100

    Added output parameters and fixed retrieval of existing infos from BPMN diagram

--- frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/bpmn-extension-element.service.ts
+++ frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/bpmn-extension-element.service.ts
@@ -8,12 +8,15 @@ export class BpmnExtensionElementService {
 
     bpmnElement: any;
     bpmnModeler: any;
+    moddle: any;
 
     constructor() { }
 
     setup(bpmnModeler: any, bpmnElement: any) {
         this.bpmnModeler = bpmnModeler;
         this.bpmnElement = bpmnElement;
+
+        this.moddle = this.bpmnModeler.get("moddle");
     }
 
     changeBpmnElement(bpmnElement: any): void {
@@ -28,6 +31,10 @@ export class BpmnExtensionElementService {
         return InputOutputHelper.getInputParameters(this.bpmnElement) as CamundaInputParameter[];
     }
 
+    getOutputParameters(): CamundaOutputParameter[] {
+        return InputOutputHelper.getOutputParameters(this.bpmnElement) as CamundaInputParameter[];
+    }
+
     /**
      * Adds an input parameter to the current bpmn element
      * @param value Value to be set as input parameter
@@ -35,33 +42,50 @@ export class BpmnExtensionElementService {
     addCamundaInputParameter(prop: BpmnProperty): void {
         const stringVal = JSON.stringify(prop.value);
 
-        // TODO: Check if there are already input parameters. If so, don't create new ones
-        const moddle = this.bpmnModeler.get("moddle");
+        const existingInputParameters = this.getInputParameters();
+        const existingOutputParameters = this.getOutputParameters();
 
-        let existingInputParameters;
-        let inputParameter;
-        let inputOutput;
-        // If this input parameter exists, set the new value
         try {
-            existingInputParameters = this.getInputParameters();
-            inputParameter = existingInputParameters.find(input => input.name == prop.key);
+            // If this input parameter exists, set the new value
+            const inputParameter = existingInputParameters.find(input => input.name == prop.key);
 
             inputParameter.value = JSON.stringify(prop.value);
-            inputOutput = moddle.create('camunda:InputOutput', {
-                inputParameters: [...existingInputParameters]
-            });
+            this.updateInputOutput(existingInputParameters, existingOutputParameters);
         } catch (error) {
             // In case it doesn't exist, create a new one
-            inputParameter = moddle.create('camunda:InputParameter', {
+            const inputParameter = this.moddle.create('camunda:InputParameter', {
                 name: `${prop.key}`,
                 value: `${stringVal}`
             });
-            inputOutput = moddle.create('camunda:InputOutput', {
-                inputParameters: [...existingInputParameters, inputParameter]
-            });
-        }
+            const inputParameters = [...existingInputParameters, inputParameter];
 
-        const extensionElements = moddle.create('bpmn:ExtensionElements', {
+            this.updateInputOutput(inputParameters, existingOutputParameters);
+        }
+    }
+
+    setCamundaOutputParameters(props: Array<BpmnProperty>): void {
+        // Get existing parameters to not overwrite them
+        const existingInputParameters = this.getInputParameters();
+        const outputParameters = new Array<CamundaOutputParameter>();
+        // Create new output (no value is set)
+        props.forEach(prop => {
+            const newOutputParameter = this.moddle.create('camunda:OutputParameter', {
+                name: `${prop.key}`,
+                //value: `${stringValue}`
+            });
+            outputParameters.push(newOutputParameter);
+        });
+
+        this.updateInputOutput(existingInputParameters, outputParameters);
+    }
+
+    updateInputOutput(inputs: Array<CamundaInputParameter>, outputs: Array<CamundaOutputParameter>): void {
+        const inputOutput = this.moddle.create('camunda:InputOutput', {
+            inputParameters: [...inputs],
+            outputParameters: [...outputs]
+        });
+
+        const extensionElements = this.moddle.create('bpmn:ExtensionElements', {
             values: [inputOutput]
         });
 
@@ -69,7 +93,6 @@ export class BpmnExtensionElementService {
             "extensionElements": extensionElements
         });
     }
-
 
     /**
      * Get an existing condition expression of the current bpmn element
@@ -105,4 +128,10 @@ class CamundaInputParameter {
     $type = "camunda:InputParameter";
     name: string;
     value: string | number | boolean | {}
+}
+
+class CamundaOutputParameter {
+    $type = "camunda:OutputParameter";
+    name: string;
+    value?: string | number | boolean | {}
 }
