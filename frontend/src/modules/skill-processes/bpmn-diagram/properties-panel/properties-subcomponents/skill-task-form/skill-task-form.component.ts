760a0c77d27872c4ca6ac8576dbbf7af4ddb6fe0
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-11-26 14:00:25 +0100

    Added output parameters and fixed retrieval of existing infos from BPMN diagram

--- frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/skill-task-form/skill-task-form.component.ts
+++ frontend/src/modules/skill-processes/bpmn-diagram/properties-panel/properties-subcomponents/skill-task-form/skill-task-form.component.ts
@@ -1,13 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Skill } from '@shared/models/skill/Skill';
 import { Isa88CommandTypeIri } from '@shared/models/state-machine/ISA88/ISA88CommandTypeIri';
 import { SkillService } from '../../../../../../shared/services/skill.service';
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, take } from 'rxjs/operators';
 import { SkillVariable } from '@shared/models/skill/SkillVariable';
 import { SkillExecutionRequestDto } from '@shared/models/skill/SkillExecutionRequest';
 import { BpmnProperty } from '../../../BpmnDataModel';
 import { BpmnExtensionElementService } from '../../bpmn-extension-element.service';
+import { firstValueFrom, Subscription } from 'rxjs';
 
 @Component({
     selector: 'skill-task-form',
@@ -15,7 +16,7 @@ import { BpmnExtensionElementService } from '../../bpmn-extension-element.servic
     styleUrls: ['./skill-task-form.component.scss']
 })
 export class SkillTaskFormComponent implements OnInit {
-    @Input() bpmnElement;
+    _bpmnElement;
 
     // Definition of the FormGroup
     fg = new FormGroup({
@@ -33,6 +34,9 @@ export class SkillTaskFormComponent implements OnInit {
     commands = Isa88CommandTypeIri;
     commandKeys;
 
+    $inoutSub: Subscription;
+    $valueSub: Subscription;
+
     @Output() basePropertyUpdated = new EventEmitter<BpmnProperty>();
 
     constructor(
@@ -42,54 +46,81 @@ export class SkillTaskFormComponent implements OnInit {
         this.commandKeys = Object.keys(Isa88CommandTypeIri);
     }
 
-
     ngOnInit(): void {
         // Set execution class as this is always the same
         const delegateClassProperty = new BpmnProperty("camunda:class", "de.hsuhh.aut.skills.bpmn.delegates.MyJavaDelegate");
         this.basePropertyUpdated.emit(delegateClassProperty);
 
+        // console.log("on init");
 
-        // Load all skills, set the first one as selected and get parameters of the selection
-        this.skillService.getAllSkills().subscribe(skills => {
-            this.skills = skills;
-            this.fg.controls.skillIri.setValue(skills[0].iri);
-            this.setupParameterForm(this.fg.controls.skillIri.value);
-        });
-
-        // Make sure parameter form matches skill
-        this.synchronizeParameterForm();
-
-        // Get current input values from the model to populate form fields in the element alraedy has a value
-        try {
-            const inputs = this.extensionElementService.getInputParameters();
-            const executionRequest = JSON.parse(inputs.find(input => input.name == "executionRequest").value as string) as SkillExecutionRequestDto;
-            // try to set the values
-            this.fg.controls.skillIri.setValue(executionRequest.skillIri);
-            this.fg.controls.commandTypeIri.setValue(executionRequest.commandTypeIri);
-            this.existingParameters = executionRequest.parameters;
-            const isSelfResetting = inputs.find(input => input.name == "isSelfResetting").value as boolean;
-            this.fg.controls.isSelfResetting.setValue(isSelfResetting);
-        } catch (error) {
-
-        }
+        // this.updateForm();
 
         // Get the current form values and store them in the process
-        this.syncFormValuesAndProcess();
+        // this.syncFormValuesAndProcess();
     }
 
     /**
      * Dynamically sets up a FormGroup for the parameters of a skill
      * @param skillIri IRI of the skill that parameters will be setup for
      */
-    setupParameterForm(skillIri: string): void {
-        // If no skillIri is given, no parameters can be set
-        if (!skillIri) return;
+    async updateForm(): Promise<void> {
+        // Clear the fg in case of switch between different skill tasks
+        this.fg.reset();
 
-        this.selectedSkill = this.skills.find(sk => sk.iri == skillIri);
+        this.skills = await firstValueFrom(this.skillService.getAllSkills());
+
+        // Get current input values from the model to populate form fields in the element alraedy has a value
+        let commandTypeIri: string;
+        let isSelfResetting: boolean;
+        try {
+            const inputs = this.extensionElementService.getInputParameters();
+            const executionRequest = JSON.parse(inputs.find(input => input.name == "executionRequest").value as string) as SkillExecutionRequestDto;
+            // try to set the values
+            this.selectedSkill = this.skills.find(skill => skill.iri === executionRequest.skillIri);
+            console.log("exReq exists, setting");
+            console.log(executionRequest);
+            console.log("selectedSkill");
+            console.log(this.selectedSkill);
+
+            commandTypeIri = executionRequest.commandTypeIri;
+            this.existingParameters = executionRequest.parameters;
+
+            isSelfResetting = inputs.find(input => input.name == "isSelfResetting").value as boolean;
+
+            console.log("done setting");
+
+        } catch (error) {
+            console.log(error);
+
+            // if no executionrequest exists
+            this.selectedSkill = this.skills[0];
+            console.log("no exReq exists, setting [0]");
+            console.log(this.selectedSkill);
+            commandTypeIri = Isa88CommandTypeIri.Start;
+            isSelfResetting = true;
+        }
+
+        this.fg.controls.skillIri.setValue(this.selectedSkill.iri);
+        this.fg.controls.commandTypeIri.setValue(commandTypeIri);
+        this.fg.controls.isSelfResetting.setValue(isSelfResetting);
+
+        // Make sure parameter form matches skill and that outputs of skill are added as task outputs
+        console.log("updating form should set input output");
+
+        this.setupParameterForm();
+        this.setOutputs();
+        this.$inoutSub = this.synchronizeInputsAndOutputs();
+        this.$valueSub = this.syncFormValuesAndProcess();
+    }
+
+
+    setupParameterForm(): void {
+
         this.selectedSkill.skillParameters.forEach(param => {
             let existingValue = "";
             try {
                 existingValue = this.existingParameters.find(exParam => exParam.name == param.name).value;
+                this.fgParameters.controls[param.name].setValue(existingValue);
             } catch (err) {
 
             }
@@ -97,22 +128,43 @@ export class SkillTaskFormComponent implements OnInit {
         });
     }
 
-
     /**
 	 * Makes sure that parameters always match to the current skill selection
 	 */
-    private synchronizeParameterForm(): void {
-        this.fg.controls.skillIri.valueChanges.subscribe(skillIri => {
-            this.setupParameterForm(skillIri);
+    private synchronizeInputsAndOutputs(): Subscription {
+        return this.fg.controls.skillIri.valueChanges.subscribe(skillIri => {
+            this.selectedSkill = this.skills.find(sk => sk.iri == skillIri);
+            // If no skillIri is given, nothing can be done
+            console.log("in synch");
+            console.log(this.selectedSkill);
+
+
+            if (!this.selectedSkill) return;
+
+            this.setupParameterForm();
+            this.setOutputs();
         });
+    }
+
+    private setOutputs(): void {
+        const bpmnOutputProperties = this.selectedSkill.skillOutputs.map(output => {
+            const outputName = `${this._bpmnElement.id}_${output.name}`;
+            return new BpmnProperty(outputName, null);
+        });
+        this.extensionElementService.setCamundaOutputParameters(bpmnOutputProperties);
     }
 
 
     /**
 	 * Subscribe to the form values and synchronize them with the process
 	 */
-    private syncFormValuesAndProcess(): void {
-        this.fg.valueChanges.pipe(debounceTime(100)).subscribe(data => {
+    private syncFormValuesAndProcess(): Subscription {
+        return this.fg.valueChanges.pipe(debounceTime(100)).subscribe(data => {
+            // Change selected skill
+            // this.selectedSkill = data.skill;
+            console.log(data);
+
+
             // Fill in parameter values and create an executionRequest
             const paramsWithValues = this.selectedSkill.skillParameters.map(param => {
                 param.value = data.parameters[param.name];
@@ -126,6 +178,27 @@ export class SkillTaskFormComponent implements OnInit {
         });
     }
 
+    // Big TODO, continue here: On resetting, the skill of the XML should be set. Only if none exists should skills [0] be set
+
+    @Input()
+    set bpmnElement(elem: any) {
+        // kill existing subscriptions
+        try {
+            this.$inoutSub.unsubscribe();
+            this.$valueSub.unsubscribe();
+        } catch (error) {
+            // If there are no subscriptions, they cant be cancelled and thats fine -> nothing to do here
+        }
+
+        this._bpmnElement = elem;
+        this.updateForm();
+        // this.selectedSkill = this.skills[0];
+        // this.fg.controls.skillIri.setValue(this.selectedSkill.iri);
+        // this.updateForm();
+        // // Make sure parameter form matches skill and that outputs of skill are added as task outputs
+        // this.synchronizeParameterFormAndOutputs();
+        // this.setOutputs();
+    }
 
     /**
 	 * Convenience getter that simplifies getting the parameter sub-FormGroup
