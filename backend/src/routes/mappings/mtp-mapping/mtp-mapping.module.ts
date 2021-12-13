e92e3659a5ae237ffd70277cc58b167da7a50801
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-12-13 09:24:01 +0100

    Extracting skills from a module so that they are  properly set up

--- backend/src/routes/mappings/mtp-mapping/mtp-mapping.module.ts
+++ backend/src/routes/mappings/mtp-mapping/mtp-mapping.module.ts
@@ -4,10 +4,12 @@ import { MtpMappingController } from './mtp-mapping.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { MtpMappingService } from './mtp-mapping.service';
 import { ModuleModule } from '../../production-modules/module.module';
+import { SkillModule } from '../../skills/skill.module';
 
 @Module({
     imports: [
         ModuleModule,
+        SkillModule,
         MulterModule.register({
             dest: './../uploaded-files/mtp',
         })],
