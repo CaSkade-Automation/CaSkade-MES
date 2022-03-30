fe524b87597ffbd86de0cae2dee26860da57c449
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-03-30 10:05:22 +0200

    Added a service locator that allows to get Angular services within data classes

--- frontend/src/app.module.ts
+++ frontend/src/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
+import { Injector, NgModule } from "@angular/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { SkillCardModule } from "./shared/modules/skill-card/skill-card.module";
+import { ServiceLocator } from "./shared/services/service-locator.service";
 
 const config = { url: 'http://localhost:9090', options: {} };
 
@@ -21,4 +22,9 @@ const config = { url: 'http://localhost:9090', options: {} };
     providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+    // Pass the app service injector to the ServiceLocator so that the locator can be used to find services by plain model classes that cannot use Angular DI
+    constructor(private injector: Injector) {
+        ServiceLocator.injector = injector;
+    }
+}
