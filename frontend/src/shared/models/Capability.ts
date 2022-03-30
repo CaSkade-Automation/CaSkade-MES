7d1cc8a2702377788255e0d506ce2d8bc5caf007
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-03-30 18:03:40 +0200

    Added getters for input / output properties

--- frontend/src/shared/models/Capability.ts
+++ frontend/src/shared/models/Capability.ts
@@ -1,8 +1,9 @@
 import { CapabilityDto } from "@shared/models/capability/Capability";
-import { FpbElement } from "@shared/models/fpb/FpbElement";
 import { RdfElement } from "@shared/models/RdfElement";
 import { ServiceLocator } from "../services/service-locator.service";
 import { SkillService } from "../services/skill.service";
+import { FpbElement } from "./FpbElement";
+import { Property } from "./Property";
 import { Skill } from "./Skill";
 
 export class Capability extends RdfElement {
@@ -14,10 +15,36 @@ export class Capability extends RdfElement {
 
     constructor(dto: CapabilityDto) {
         super(dto.iri);
-        this.inputs = dto.inputs.map(input => new FpbElement(input.iri));
-        this.outputs = dto.outputs.map(output => new FpbElement(output.iri));
+        this.inputs = dto.inputs.map(inputDto => new FpbElement(inputDto));
+        this.outputs = dto.outputs.map(outputDto => new FpbElement(outputDto));
         dto.skillIris?.forEach(skillIri => {
             this.skillService.getSkillByIri(skillIri).subscribe(data => this.skills.push(data));
         });
+    }
+
+    /**
+     * Convenience getter to get all input properties
+     */
+    get inputProperties(): Array<Property>  {
+        const init = new Array<Property>();
+        const inputProperties = this.inputs.reduce((previousInput, currentInput) => {
+            init.push(...currentInput.properties);
+            return init;
+        }, init);
+
+        return inputProperties;
+    }
+
+    /**
+     * Convenience getter to get all output properties
+     */
+    get outputProperties(): Array<Property>  {
+        const init = new Array<Property>();
+        const outputProperties = this.outputs.reduce((previousOutput, currentOutput) => {
+            init.push(...currentOutput.properties);
+            return init;
+        }, init);
+
+        return outputProperties;
     }
 }
