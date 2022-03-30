d6f9c6f8d157f7718ded0e2b0751ea0a4d245c3e
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-03-30 09:59:54 +0200

    inputs and outputs are both direct children of capabilities

--- backend/src/routes/capabilities/capability-mappings.ts
+++ backend/src/routes/capabilities/capability-mappings.ts
@@ -10,15 +10,15 @@ const capabilityMapping: MappingDefinition[] = [
             {
                 rootName: 'inputs',
                 propertyToGroup: 'input',
-                name: 'inputs',
-                childMappings: [
-                    {
-                        rootName: 'outputs',
-                        propertyToGroup: 'output',
-                        name: 'outputs',
-                    }
-                ]
+                name: 'iri',
+                toCollect:['input']
             },
+            {
+                rootName: 'outputs',
+                propertyToGroup: 'output',
+                name: 'iri',
+                toCollect:['output']
+            }
         ]
     },
 
