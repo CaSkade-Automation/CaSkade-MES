3d5174384a1f63e3de38f1334d31bbd9ce9ef1df
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-09-26 17:31:47 +0200

    Properly loading both a single as well as all modules as a graph

--- frontend/src/modules/graph-visualization/graph-visualization.component.ts
+++ frontend/src/modules/graph-visualization/graph-visualization.component.ts
@@ -6,8 +6,7 @@ import * as d3Scale from 'd3-scale';
 import * as d3ScaleChromatic from'd3-scale-chromatic';
 import * as d3Drag from'd3-drag';
 import { ActivatedRoute } from '@angular/router';
-import { NodeCreatorService } from './node-creator.service';
-import { D3GraphData, D3Link, D3Node, NodeType } from './D3GraphData';
+import { D3GraphData, D3Link, D3Node, D3Serializable, NodeType } from './D3GraphData';
 import { ModuleService } from '../../shared/services/module.service';
 import { QueryService } from '../../shared/services/query.service';
 import { RdfElement } from '../../../../shared/src/models/RdfElement';
@@ -57,7 +56,7 @@ export class GraphVisualizationComponent implements AfterViewInit {
     ) {}
 
     @ViewChild('g') svgContainer: ElementRef;
-    svgHeight = 100;
+    svgHeight = 200;
     svgWidth = 200;
     moduleIri: string;
 
@@ -66,23 +65,26 @@ export class GraphVisualizationComponent implements AfterViewInit {
         this.svgHeight = this.svgContainer.nativeElement.offsetHeight;
         this.route.params.subscribe(p => {
             this.moduleIri = p['moduleName'];
+            if(!this.moduleIri) {
+                this.moduleService.getAllModules().subscribe(modules => this.setupSimulation(modules));
+            } else {
+                this.moduleService.getModuleByIri(this.moduleIri).subscribe(module => this.setupSimulation([module]));
+            }
         });
 
-        this.moduleService.getAllModules().subscribe(modules => {
-            const data = new D3GraphData();
-            modules.forEach(module => data.addData(module.toD3GraphData()));
-            this.data = data;
-            this.setSvg();
-            this.setSimulation();
-        });
     }
 
     /**Defines settings of the SVG and the color-scheme of the nodes */
-    setSvg(): void {
+    setupSimulation(data: D3Serializable []): void {
+        // prepare data
+        const graphData = new D3GraphData();
+        data.forEach(module => graphData.addData(module.toD3GraphData()));
+        this.data = graphData;
+
+        // set svg
         this.svg = d3Selection.select("#graph")
             .attr("width",this.svgWidth + 65)
             .attr("height", this.svgHeight + 65);
-        // this.svg = d3Selection.select("#graph")   // size definitions for the svg
 
         this.color = d3Scale.scaleOrdinal(d3ScaleChromatic.schemeCategory10); // chooses a scheme category for node colours
 
@@ -105,17 +107,16 @@ export class GraphVisualizationComponent implements AfterViewInit {
         // Define elemennt containers. Important nodes after links so that nodes are rendered on top
         this.linksContainer = this.svg.append('g').attr('class', 'linksContainer');
         this.nodesContainer = this.svg.append('g').attr('class', 'nodesContainer');
-    }
 
-    /**Defines settings of the simulation */
-    setSimulation(): void {
+        // set Simulation
+
         this.simulation = d3Force.forceSimulation(this.data.nodes)
             .force("link", d3Force.forceLink()
                 .id((node: D3Node) => node.id)
                 .distance(150)
                 .links(this.data.links))
             .force("collision", d3Force.forceCollide(this.nodeRadius*1.5))
-            .force("charge", d3Force.forceManyBody().strength(-300)) // node magnetism / attraction
+            .force("charge", d3Force.forceManyBody().strength(-60)) // node magnetism / attraction
             .force("center", d3Force.forceCenter(this.svgWidth/ 2, this.svgHeight / 2)) //Zentrierung der Nodes
             .on("tick", this.ticked); // tick on every step of the simulation
 
@@ -154,13 +155,11 @@ export class GraphVisualizationComponent implements AfterViewInit {
         this.links = this.linksContainer.selectAll(".links").data(this.data.links);
 
         const linkEnter=this.links.enter()
-            .append("line")
+            .append("path")
+            .attr("fill", "transparent")
             .style("stroke", "#555")
             .attr("class", "links")
-            .attr("x2", (d) => { return d.source.x; })
-            .attr("y2", (d) => { return d.source.y; })
-            .attr("x1", (d) => { return d.target.x; })
-            .attr("x1", (d) => { return d.target.y; })
+            .attr("d", (l: D3Link) => this.positionLinks(l))
             .attr('marker-end', 'url(#arrowhead)') // arrow on end of the link
         ;
 
@@ -200,62 +199,51 @@ export class GraphVisualizationComponent implements AfterViewInit {
     ticked = (): void => {
         this.nodes
             .attr("cx", (d: D3Node) => {
-                d.x = Math.max(0 + this.nodeRadius, Math.min(d.x, this.svgWidth - 5 * this.nodeRadius));
-                const relativeDragX = 100 * d.x / this.svgWidth;
-                return relativeDragX + '%';
+                if (d.x < 0) return 30;
+                if (d.x > this.svgWidth) return this.svgWidth - 30;
+                else return d.x;
             })
             .attr("cy", (d: D3Node) => {
-                d.y = Math.max(0 + this.nodeRadius, Math.min(d.y, this.svgHeight - 5 * this.nodeRadius));
-                const relativeDragY = 100 * d.y / this.svgHeight;
-                return relativeDragY + '%';
+                if (d.y < 0) return 30;
+                if (d.y > this.svgHeight) return this.svgHeight - 30;
+                else return d.y;
             });
 
         this.links
-            .attr("x1", (l: D3Link) => {  // restrictions for link positions in the svg
-                l.source.x = Math.max(0 + this.nodeRadius, Math.min(l.source.x, this.svgWidth - 5 * this.nodeRadius));
-                const relativeSourceX = 100 * l.source.x / this.svgWidth;
-                return relativeSourceX + '%';
-            })
-            .attr("y1", (l: D3Link) => {
-                l.source.y = Math.max(0 + this.nodeRadius, Math.min(l.source.y, this.svgHeight - 5 * this.nodeRadius));
-                const relativeSourceY = 100 * l.source.y / this.svgHeight;
-                return relativeSourceY + '%';
-            })
-            .attr("x2", (l: D3Link) => {
-                l.target.x = Math.max(0 + this.nodeRadius, Math.min(l.target.x, this.svgWidth - 5 * this.nodeRadius));
-                const relativeTargetX = 100 * l.target.x / this.svgWidth;
-                return relativeTargetX + '%';
-            })
-            .attr("y2", (l: D3Link) => {
-                l.target.y = Math.max(0 + this.nodeRadius, Math.min(l.target.y, this.svgHeight - 5 * this.nodeRadius));
-                const relativeTargetY = 100 * l.target.y / this.svgHeight;
-                return relativeTargetY + '%';
-            });
+            .attr("d", (l: D3Link) => this.positionLinks(l));
 
         this.texts
-            .attr("dx", (d: D3Node) => {    // restrictions for node text positions in the svg
-                const relativeRadius = 100 * this.nodeRadius / this.svgWidth;
-                const relativeDragX = 100 * d.x / this.svgWidth;
-                return (relativeDragX + relativeRadius) + '%';
-            })
-            .attr("dy", (d: D3Node) => {
-                const relativeRadius = 100 * this.nodeRadius / this.svgHeight;
-                const relativeDragY = 100 * d.y / this.svgHeight;
-                return (relativeDragY - 0.5*relativeRadius) + '%';
-            });
+            .attr("dx", (d: D3Node) => d.x + 15)
+            .attr("dy", (d: D3Node) => d.y + 15);
 
         this.linkTexts
             .attr("dx", (l: D3Link) => {  // restrictions for link text positions in the svg
                 const midX = (l.target.x + l.source.x) / 2 - 10;
-                const relativeMidX = 100 * midX / this.svgWidth;
-                return  relativeMidX + "%";
+                return  midX;
             })
             .attr("dy", (l: D3Link) => {
                 const midY = (l.target.y + l.source.y) / 2;
-                const relativeMidY = 100 * midY / this.svgHeight;
-                return  relativeMidY + "%";
+                return  midY;
             });
 
+    }
+
+    private positionLinks(l: D3Link) {
+        const offset = 30;
+
+        const midpointX = (l.source.x + l.target.x) / 2;
+        const midpointY = (l.source.y + l.target.y) / 2;
+
+        const dx = (l.target.x - l.source.x);
+        const dy = (l.target.y - l.source.y);
+
+        const normalise = Math.sqrt((dx * dx) + (dy * dy));
+        const offSetX = midpointX + offset*(dy/normalise);
+        const offSetY = midpointY - offset*(dx/normalise);
+
+        return "M" + l.source.x + "," + l.source.y +
+                "S" + offSetX + "," + offSetY +
+                " " + l.target.x + "," + l.target.y;
     }
 
     private convertSpecialChars(iri: string): string {
@@ -327,6 +315,8 @@ export class GraphVisualizationComponent implements AfterViewInit {
         this.queryService.getNeighbors(d.id).subscribe(neighborData => {
             const numberOfNeighbors = neighborData.length;
             const distance = 15;
+
+            // Create nodes and links for data and position symmetrically
             neighborData.forEach(nD => {
                 let i = 0;
                 const angle = (360/numberOfNeighbors * i) * (Math.PI/180);
