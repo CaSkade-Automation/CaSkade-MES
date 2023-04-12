da9697f13988ab465ced90e0d4fdb3de05cfba79
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-04-12 16:43:40 +0200

    feat(frontend): :sparkles: warning messages and modal for GraphDB
    adds warning messages for APIs that are not connected as well as a very prominent modal in case no GraphDB instance is running

--- frontend/src/modules/dashboard/components/settings-overview/settings-overview.component.ts
+++ frontend/src/modules/dashboard/components/settings-overview/settings-overview.component.ts
@@ -1,11 +1,13 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { ProcessDefinitionService } from '../../../../shared/services/bpmn/process-definition.service';
 import { GraphDbRepoService } from '../../../../shared/services/graphDbRepoService.service';
 import { MtpMappingService } from '../../../../shared/services/mtp-mapping.service';
 import { PlcMappingService } from '../../../../shared/services/plc-mapping.service';
+import { MessageService } from '../../../../shared/services/message.service';
+import {Modal} from "bootstrap";
 
 @Component({
     selector: 'settings-overview',
@@ -13,6 +15,8 @@ import { PlcMappingService } from '../../../../shared/services/plc-mapping.servi
     styleUrls: ['./settings-overview.component.scss']
 })
 export class SettingsOverviewComponent implements OnInit {
+
+    @ViewChild('graphDbWarningModal') graphDbWarningModal: ElementRef;
 
     apiConnectionForm = this.fb.group({
         graphDbConnected: this.fb.control({value: false, disabled: true}),
@@ -28,22 +32,32 @@ export class SettingsOverviewComponent implements OnInit {
         private processService: ProcessDefinitionService,
         private mtpMappingService: MtpMappingService,
         private plcMappingService: PlcMappingService,
+        private messageService: MessageService
     ){
 
     }
 
     ngOnInit(): void {
-        this.setValue(this.graphDbService.isConnected(), this.apiConnectionForm.controls.graphDbConnected);
-        this.setValue(this.processService.isConnected(), this.apiConnectionForm.controls.processServiceConnected);
-        this.setValue(this.mtpMappingService.isConnected(), this.apiConnectionForm.controls.mtpMappingServiceConnected);
-        this.setValue(this.plcMappingService.isConnected(), this.apiConnectionForm.controls.plcMappingServiceConnected);
+        this.setValue("GraphDB", this.graphDbService.isConnected(), this.apiConnectionForm.controls.graphDbConnected);
+        this.setValue("BPMN Engine", this.processService.isConnected(), this.apiConnectionForm.controls.processServiceConnected);
+        this.setValue("MTP Mapping Service", this.mtpMappingService.isConnected(), this.apiConnectionForm.controls.mtpMappingServiceConnected);
+        this.setValue("PLC Mapping Service",this.plcMappingService.isConnected(), this.apiConnectionForm.controls.plcMappingServiceConnected);
     }
 
-    setValue(obs: Observable<HttpResponse<any>>, fC: FormControl): void {
+    setValue(apiName: string, obs: Observable<HttpResponse<any>>, fC: FormControl): void {
         obs.subscribe({
             next: (val) => fC.setValue(true),
-            error: (err) => fC.setValue(false)
+            error: (err) => {
+                fC.setValue(false);
+                if(apiName == "GraphDB") this.showGraphDbWarningModal();
+                else this.messageService.danger(`${apiName} not connected`, `No connection to ${apiName}. Make sure to start this service to use its features.`);
+            }
         });
-        this.apiConnectionForm.get("graphDbConnected").value;
+    }
+
+    showGraphDbWarningModal(): void {
+        const elem = this.graphDbWarningModal.nativeElement;
+        const myModal = new Modal(elem);
+        myModal.show();
     }
 }
