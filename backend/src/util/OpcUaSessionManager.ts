956b2894031ef6f9518f716f9cdcb2389204ffa3
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-06-05 13:32:12 +0200

    feat(backend): :bug: new OPC UA ontology IRI, endpoint selection
    Properly selects a suitable endpoint

--- backend/src/util/OpcUaSessionManager.ts
+++ backend/src/util/OpcUaSessionManager.ts
@@ -1,9 +1,9 @@
 import { ClientSession, ConnectionStrategy, MessageSecurityMode, OPCUAClient,
-    OPCUAClientOptions, SecurityPolicy, UserIdentityInfo, UserTokenType } from "node-opcua";
+    OPCUAClientOptions, SecurityPolicy, UserIdentityInfo, UserNameIdentityToken, UserTokenType } from "node-opcua";
 import { Injectable } from "@nestjs/common";
 import { GraphDbConnectionService } from "./GraphDbConnection.service";
 import { SparqlResultConverter } from "sparql-result-converter";
-import { opcUaSkillParameterMapping } from "../routes/skill-execution/executors/skill-execution-mappings";
+import { opcUaServerInfoMapping, opcUaSkillParameterMapping } from "../routes/skill-execution/executors/skill-execution-mappings";
 
 @Injectable()
 export class OpcUaSessionManager {
@@ -12,14 +12,16 @@ export class OpcUaSessionManager {
     private sessions = new Map<string, Promise<ClientSession>>();
     private connectionStrategy: ConnectionStrategy = {
         initialDelay: 1000,
-        maxRetry: 1,
+        maxRetry: 2,
         maxDelay: 10000,
-        randomisationFactor: 0.5
+        randomisationFactor: 0.5,
     };
 
     constructor(
         private graphDbConnection: GraphDbConnectionService
-    ){}
+    ){
+        this.setupGarbageCollector();
+    }
 
     public getSession(skillIri: string): Promise<ClientSession> {
         let session = this.sessions.get(skillIri);
@@ -35,18 +37,27 @@ export class OpcUaSessionManager {
 
     private async createSession(skillIri: string): Promise<ClientSession> {
         const uaServerInfo = await this.getOpcUaServerInfo(skillIri);
-        const options = this.createOptionsObject(uaServerInfo.messageSecurityMode, uaServerInfo.securityPolicy);
+        const endpoint = this.findSuitableEndpoint(uaServerInfo);
+        const options = this.createOptionsObject(endpoint.messageSecurityMode, endpoint.securityPolicy);
         const uaClient = OPCUAClient.create(options);
 
         try {
-            await uaClient.connect(uaServerInfo.endpointUrl);
+            await uaClient.connect(endpoint.endpointUrl);
         } catch (err) {
-            console.log(`Error while connecting to UAServer at ${uaServerInfo.endpointUrl}. Err: ${err}`);
+            console.log(`Error while connecting to endpoint of UAServer at ${endpoint.endpointUrl}. Err: ${err}`);
         }
 
-        const userInfo = this.createUserIdentityToken(uaServerInfo.username, uaServerInfo.password);
-        const uaSession = await uaClient.createSession(userInfo);             // TODO: Integrate user identity token here
+        const userInfo = this.findUserIdentityInfo(endpoint);
+        const uaSession = await uaClient.createSession(userInfo);
         return uaSession;
+    }
+
+    private findSuitableEndpoint(uaServerInfo: OpcUaServerInfo): EndpointDescription {
+        return uaServerInfo.endpoints.find(endpoint => {
+            const messageSecurityMode = this.getMessageSecurityMode(endpoint.messageSecurityMode);
+            const securityPolicy = this.getSecurityPolicy(endpoint.securityPolicy);
+            return (messageSecurityMode == MessageSecurityMode.None && securityPolicy == SecurityPolicy.None);
+        });
     }
 
 
@@ -57,27 +68,32 @@ export class OpcUaSessionManager {
     private async getOpcUaServerInfo(skillIri: string): Promise<OpcUaServerInfo> {
         const query = `
         PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
-        PREFIX OpcUa: <http://www.hsu-ifa.de/ontologies/OpcUa#>
-        SELECT ?endpointUrl ?messageSecurityMode ?securityPolicy ?username ?password WHERE {
+        PREFIX OpcUa: <http://www.w3id.org/hsu-aut/OpcUa#>
+        SELECT ?serverIri ?endpoint ?endpointUrl ?messageSecurityMode ?securityPolicy ?userIdentityToken ?tokenType
+            ?userName ?password WHERE {
             BIND(<${skillIri}> AS ?skillIri).
             ?skillIri CSS:accessibleThrough ?skillInterface.
-            ?uaServer OpcUa:hasNodeSet/OpcUa:containsNode ?skillInterface;
-                OpcUa:hasEndpointUrl ?endpointUrl;
+            ?serverIri OpcUa:hasNodeSet/OpcUa:containsNode ?skillInterface;
+                OpcUa:hasEndpointDescription ?endpoint.
+            ?endpoint OpcUa:hasEndpointUrl ?endpointUrl;
                 OpcUa:hasMessageSecurityMode ?messageSecurityMode;
-                OpcUa:hasSecurityPolicy ?securityPolicy.
+                OpcUa:hasSecurityPolicy ?securityPolicy;
+                OpcUa:hasUserIdentityToken ?userIdentityToken.
+            ?userIdentityToken a ?tokenType.
+            ?tokenType rdfs:subClassOf OpcUa:UserIdentityToken.
             OPTIONAL {
-                ?uaServer OpcUa:requiresUserName ?username;
+                ?userIdentityToken OpcUa:requiresUserName ?userName;
                     OpcUa:requiresPassword ?password.
             }
         }`;
         const queryResult = await this.graphDbConnection.executeQuery(query);
-        const opcUaServerInfo = <unknown>this.converter.convertToDefinition(queryResult.results.bindings, opcUaSkillParameterMapping)
+        const opcUaServerInfo = <unknown>this.converter.convertToDefinition(queryResult.results.bindings, opcUaServerInfoMapping)
             .getFirstRootElement()[0] as Promise<OpcUaServerInfo>;
 
         return opcUaServerInfo;
     }
 
-    createOptionsObject(messageSecurityModeIri: string, securityPolicyIri: string): OPCUAClientOptions {
+    private createOptionsObject(messageSecurityModeIri: string, securityPolicyIri: string): OPCUAClientOptions {
         const options : OPCUAClientOptions = {
             applicationName: "SkillMEx OPC UA Capability Executor",
             connectionStrategy: this.connectionStrategy,
@@ -108,51 +124,66 @@ export class OpcUaSessionManager {
 
     protected getSecurityPolicy(securityPolicyString: string): SecurityPolicy {
         switch (securityPolicyString) {
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Aes128_Sha256_RsaOaep":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Aes128_Sha256_RsaOaep":
             return SecurityPolicy.Aes128_Sha256_RsaOaep;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic128":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic128":
             return SecurityPolicy.Basic128;
         case "OpcUa:SecurityPolicy_Basic128":
             return SecurityPolicy.Basic128Rsa15;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic192":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic192":
             return SecurityPolicy.Basic192;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic192Rsa15":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic192Rsa15":
             return SecurityPolicy.Basic192Rsa15;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic256":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic256":
             return SecurityPolicy.Basic256;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic256Rsa15":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic256Rsa15":
             return SecurityPolicy.Basic256Rsa15;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_Basic256Sha256":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_Basic256Sha256":
             return SecurityPolicy.Basic256Sha256;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_None":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_None":
             return SecurityPolicy.None;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_PubSub_Aes128_CTR":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_PubSub_Aes128_CTR":
             return SecurityPolicy.PubSub_Aes128_CTR;
-        case "http://www.hsu-ifa.de/ontologies/OpcUa#SecurityPolicy_PubSub_Aes256_CTR":
+        case "http://www.w3id.org/hsu-aut/OpcUa#SecurityPolicy_PubSub_Aes256_CTR":
             return SecurityPolicy.PubSub_Aes256_CTR;
         default:
             return SecurityPolicy.None;
         }
     }
 
-    protected createUserIdentityToken(user: string, password: string): UserIdentityInfo {
-        // if a user and password are set -> create a token
-        if(user && password) {
+    protected findUserIdentityInfo(endpoint: EndpointDescription): UserIdentityInfo {
+        // see if there is a userName token -> use it
+        let token = endpoint.userIdentityTokens.find(token => token.tokenType == "http://www.w3id.org/hsu-aut/OpcUa#UserNameIdentityToken");
+        if (token) {
             const info: UserIdentityInfo = {
-                userName: user,
-                password: password,
+                userName: token.userName,
+                password: token.password,
                 type: UserTokenType.UserName
             };
             return info;
-        } else {
+        }
+
+        // if not, take an anonymous one
+        token = endpoint.userIdentityTokens.find(token => token.tokenType == "http://www.w3id.org/hsu-aut/OpcUa#AnonymousIdentityToken");
+        if(token) {
             const info: UserIdentityInfo = {
                 type: UserTokenType.Anonymous
             };
             return info;
         }
+
+        throw new Error("Cannot find endpoint with supported user identity info.");     // if no anonymous either, throw an error
+        // TODO: Implement other types of user tokens
     }
 
-    protected async endUaConnection(skillIri: string): Promise<void> {
+    /**
+     * Very simple session invalidation, just delete everything after 10 seconds
+     */
+    private setupGarbageCollector(): void {
+        setInterval(() => this.sessions.clear(), 10000);
+    }
+
+    private async endUaConnection(skillIri: string): Promise<void> {
         const uaSession = await this.sessions.get(skillIri);
         uaSession.close().then(() => this.sessions.delete(skillIri));
 
@@ -164,10 +195,26 @@ export class OpcUaSessionManager {
 }
 
 
-interface OpcUaServerInfo{
+class OpcUaServerInfo {
+    serverIri: string;
+    endpoints = new Array<EndpointDescription>();
+}
+
+class EndpointDescription {
     endpointUrl: string;
     messageSecurityMode: string;
     securityPolicy: string;
-    username: string;
-    password: string;
+    userIdentityTokens = new Array<UserIdentityToken>();
 }
+
+class UserIdentityToken {
+    tokenType: OpcUaOntologyTokenType
+    userName?: string;
+    password?: string;
+}
+
+type OpcUaOntologyTokenType =
+    "http://www.w3id.org/hsu-aut/OpcUa#AnonymousIdentityToken" |
+    "http://www.w3id.org/hsu-aut/OpcUa#IssuedIdentityToken" |
+    "http://www.w3id.org/hsu-aut/OpcUa#UserNameIdentityToken" |
+    "http://www.w3id.org/hsu-aut/OpcUa#X509IdentityToken";
