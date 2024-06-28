ed0109b6f2a6ca638a3e955f7e78f82e26db20c4
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-06-28 09:46:05 +0200

    feat(frontend): :sparkles: adds dynamic loading of LLMs

--- frontend/src/shared/modules/ontology-registration/llm-generation/llm-generation.component.ts
+++ frontend/src/shared/modules/ontology-registration/llm-generation/llm-generation.component.ts
@@ -1,20 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
-import { CapabilityService } from 'src/shared/services/capability.service';
-import { ModuleService } from 'src/shared/services/module.service';
-import { SkillService } from 'src/shared/services/skill.service';
 import { MessageService } from '../../../services/message.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { LlmGenerationService } from '../../../services/llm-generation.service';
 
 @Component({
     selector: 'llm-generation',
     templateUrl: './llm-generation.component.html',
     styleUrls: ['./llm-generation.component.scss']
 })
-export class LlmGenerationComponent {
+export class LlmGenerationComponent implements OnInit {
     @Input() context: string
 
-    models = new Array<string>("asd", "asd");
+    models = new Array<string>();
     form = this.fb.group({
         model: this.fb.control(this.models[0], Validators.required),
         taskDescription: this.fb.control("", Validators.required)
@@ -22,9 +19,15 @@ export class LlmGenerationComponent {
 
     constructor(
         private messageService: MessageService,
-        private capabilityService: CapabilityService,
+        private llmGeneration: LlmGenerationService,
         private fb: FormBuilder
     ) { }
+
+    ngOnInit(): void {
+        this.llmGeneration.getModels().subscribe(models => {
+            this.models = models;
+        });
+    }
 
     submit(): void {
         if(this.context=="production-modules"){
@@ -35,6 +38,11 @@ export class LlmGenerationComponent {
         }
         if(this.context == "capabilities") {
             this.messageService.success("Works", "it works");
+            const {model, taskDescription} = this.form.value;
+            console.log(model, taskDescription);
+
+            this.llmGeneration.generateCapability(taskDescription, model).subscribe(data => console.log(data));
+
             // this.capabilityService.addCapability(this.ontologyString).pipe(take(1)).subscribe(
             //     () => this.ontologyString="Ontology registered"
             // );
